a899012ce65d413447a15961ecbc26b912cd429f
Esa I <relationsou@hotmail.com>
2021-02-08 22:03:59 +0200

    2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '050514887799'},
+      { name: 'Johanna Tukiainen',
+      number: '0400123123'},
+      { name: 'Sandra',
+      number: '0760544510'},
+      { name: 'Kimi Räikkönen',
+      number: '09102232151'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -13,6 +22,7 @@ const App = () => {
     } else {
       const personObject = {
         name: newName,
+        number: newNumber
       }
       
       setPersons(persons.concat(personObject))
@@ -24,6 +34,20 @@ const App = () => {
     setNewName(event.target.value)  
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const searchWithFilter = (event) => {
+
+    // sinänsä, riittäisikö se, että filtteröidään vain tuossa alla nuo tiedot? 
+    // heitetäänkö ulos ja vähän siistimmäksi? 
+    setFilter(event.target.value)
+  }
+
+  const showFilteredPersons = 
+    persons.filter(person => person.name.includes(filter) )
+  
    
 
   return (
@@ -31,16 +55,26 @@ const App = () => {
     <div>
      
       <h2>Phonebook</h2>
+
+      filter shown with<input value={filter} onChange={searchWithFilter}></input>
+
+      <h2>add a new</h2>
       <form onSubmit={addNumber}>
         <div>
-          <input value={newName} onChange={handleNameChange}></input> 
+          name: <input value={newName} onChange={handleNameChange}></input> 
+          <br />
+          number: <input value={newNumber} onChange={handleNumberChange}></input>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <ul key={person.name}> {person.name} </ul>)}
+      {console.log(filter)}
+      {showFilteredPersons.map(person => <ul key={person.name}> {person.name} :  
+      { person.number} </ul>) }
+
+
       ...
     </div>
   )
